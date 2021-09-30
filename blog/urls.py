c9bbafbac621e10c1e4a30d31781a3cdf74d2c6e
stevescilar <stevemuambi@gmail.com>
2021-09-30 17:08:48 +0300

    we moving

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path('', views.all_blogs, name='all_blogs'),
     path('', views.main_blog, name='main_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Post,Blog
 
 
@@ -7,5 +7,9 @@ def all_blogs(request):
     return render(request, 'blog/all_blogs.html',{'posts':posts})
 
 def main_blog(request):
-    blogs = Blog.objects.order_by('-date')[:3]
+    blogs = Blog.objects.order_by('-date')
     return render(request,'blog/main_blog.html',{'blogs':blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{'blog':blog})
